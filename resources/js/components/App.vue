2dabf82628a39ec3623ac156a15f659e78328027
vera.sprinceana <sprinceana.vera96@gmail.com>
2022-06-15 18:47:09 +0200

    post-component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
+    <div>
+        <Header/>
+        <div class="container">
+            <PostList/>
+        </div>
+    </div>
+
+    <!-- <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
                     <Header/>
@@ -9,24 +15,27 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </div> -->
+
 </template>
 
 <script>
-import Header from './Header.vue';
+    import Header from './Header.vue';
+    import PostList from './posts/PostList.vue';
+    export default {
+        name: 'App',
+        components: {
+            Header,
+            PostList
+        },
+        
+    }
 
-export default{
-    name: 'App',
-   components:{
-       Header
-   }
-}
 </script>
 
 <style scoped>
-.text-danger{
-    color: red;
-}
+    .text-danger {
+        color: red;
+    }
 
 </style>
